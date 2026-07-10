29e749e2aedd4e6cefe8965fc3298a6626e47c23
MAZHAR <iiestshibpur605@gmail.com>
2026-07-10 21:26:04 +0530

    Added YouTube Link

--- Binary Lifting/Path Existence Queries in a Graph II.cpp
+++ Binary Lifting/Path Existence Queries in a Graph II.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code as well    */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=QL8ZNyBOzp0
     Company Tags                : Will update later
     Leetcode Link               : https://leetcode.com/problems/path-existence-queries-in-a-graph-ii/
 */
